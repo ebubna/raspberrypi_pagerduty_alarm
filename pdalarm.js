f5dd40d0d2a9faa03a0745e01af0c4493d6cfd2c
Eric Bubna <ebubna@gmail.com>
2023-02-04 12:28:19 -0800

    Create pdalarm.js

---
+++ pdalarm.js
@@ -0,0 +1,47 @@
+const https = require('https');
+const fs = require('fs');
+const child_process = require('child_process');
+
+const options = {
+  hostname: 'api.pagerduty.com',
+  port: 443,
+  path: '/incidents?sort_by=created_at:desc',
+  method: 'GET',
+  headers: {
+    Accept: 'application/vnd.pagerduty+json;version=2',
+    Authorization: 'Token token=YOUR-PAGERDUTY-TOKEN'
+  }
+};
+
+const checkForIncidents = () => {
+  https.get(options, (res) => {
+    let data = '';
+
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      const incidents = JSON.parse(data).incidents;
+      if (incidents.length > 0) {
+        const mostRecentIncident = incidents[0];
+        console.log(mostRecentIncident.summary);
+
+        // Play an MP3 file if there is a current incident
+        child_process.exec('mpg123 alarm.mp3', (err, stdout, stderr) => {
+          if (err) {
+            console.error(err);
+          }
+        });
+      } else {
+        console.log('No incidents found.');
+      }
+    });
+  }).on('error', (err) => {
+    console.error(err);
+  });
+};
+
+setInterval(checkForIncidents, 60 * 1000); // Check every 60 seconds
+
+checkForIncidents(); // Perform initial check
